8e8a4a73f3dbbacd84437cede69c2bfa2bda8b58
luiz.vitorino <luiz.vitorino@iteris.com.br>
2024-04-21 09:18:05 -0300

    segundo commit

--- src/components/LocationSection.js
+++ src/components/LocationSection.js
@@ -11,8 +11,8 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const foto1 = "/foto1.png"; // Verifique se esta imagem está na pasta public
-const foto2 = "/foto2.png"; // Verifique se esta imagem está na pasta public
+const foto1 = "/foto1.png";
+const foto2 = "/foto2.png";
 
 const settings = {
   dots: true,
@@ -27,40 +27,30 @@ function LocationSection() {
   const locationImages = [foto1, foto2];
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
-
-  // O src do mapa
   const mapSrc =
     "https://www.google.com/maps/embed/v1/place?key=AIzaSyB1Vkg3EYIdK1Z933UWCmMEkOoHDiOixQU&q=R.%20Paulo%20Moriyama%2C%20139%20-%20Vila%20Figueira%2C%20Suzano%20-%20SP%2C%2008676-080";
 
   return (
-    <Box id="location" sx={{ mt: 8, mb: 4 }}>
+    <Box id="location" sx={{ overflow: "hidden", mt: 8, mb: 4 }}>
       <Typography variant="h4" gutterBottom align="center" sx={{ mb: 2 }}>
         Nossa Localização
       </Typography>
-      <Grid container spacing={2} justifyContent="space-around">
+      <Grid container spacing={2} justifyContent="center">
         <Grid item xs={12} md={6}>
           <Slider {...settings}>
             {locationImages.map((src, index) => (
-              <div
-                key={index}
-                style={{
-                  position: "relative",
-                  paddingBottom: isMobile ? "56.25%" : "75%",
-                }}
-              >
+              <Paper key={index} elevation={4}>
                 <img
                   src={src}
                   alt={`Localização ${index + 1}`}
                   style={{
-                    // position: "absolute",
-                    top: 0,
-                    left: 0,
                     width: "100%",
-                    height: isMobile ? "50vh" : "75vh",
+                    height: "auto",
+                    maxHeight: isMobile ? "50vh" : "75vh",
                     objectFit: "cover",
                   }}
                 />
-              </div>
+              </Paper>
             ))}
           </Slider>
         </Grid>
